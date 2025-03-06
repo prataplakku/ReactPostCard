2884a8d4e1253c092ec23e65410623651a9a9af5
prataplakku <prataplakku@gmail.com>
2025-03-05 22:13:43 -0500

    first commit

---
+++ src/app/PostCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostCard = ({(props): {name: string, index: number}}) => {
+  return (
+    <div className="PostCard">
+
+    </div>
+  )
+}
+
+export default PostCard
+
---
+++ src/app/PostList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostList = () => {
+    return (
+        <div className="PostList">
+            Postlist
+        </div>
+    )
+}
+
+export default PostList
